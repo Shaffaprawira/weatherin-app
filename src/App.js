9f495e4a5da9988e144b1582026ec99354c0e073
Mochammad Shaffa Prawiranegara <mochammad.shaffa@ui.ac.id>
2023-09-25 05:01:59 +0700

    fix App.css

--- src/App.js
+++ src/App.js
@@ -23,37 +23,37 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* HEADER  */}
-        <h1>Weather App</h1>
-
-        {/* Search Box - Input + Button  */}
-        <div>
-          <input
-            type="text"
-            placeholder="Enter city/town..."
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button onClick={searchPressed}>Search</button>
-        </div>
-
-        {/* If weather is not undefined display results from API */}
-        {typeof weather.main !== "undefined" ? (
-          <div>
-            {/* Location  */}
-            <p>{weather.name}</p>
-
-            {/* Temperature Celsius  */}
-            <p>{weather.main.temp}°C</p>
-
-            {/* Condition (Sunny ) */}
-            <p>{weather.weather[0].main}</p>
-            <p>({weather.weather[0].description})</p>
+      <div className="App-container">
+        <header className="App-header">
+          <div className="App-content">
+            {/* HEADER  */}
+            <h1>Weather App</h1>
+            {/* Search Box - Input + Button  */}
+            <div>
+              <input
+                type="text"
+                placeholder="Enter city/town..."
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button onClick={searchPressed}>Search</button>
+            </div>
+            {/* If weather is not undefined display results from API */}
+            {typeof weather.main !== "undefined" ? (
+              <div>
+                {/* Location  */}
+                <p>{weather.name}</p>
+                {/* Temperature Celsius  */}
+                <p>{weather.main.temp}°C</p>
+                {/* Condition (Sunny ) */}
+                <p>{weather.weather[0].main}</p>
+                <p>({weather.weather[0].description})</p>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-        ) : (
-          ""
-        )}
-      </header>
+        </header>
+      </div>
     </div>
   );
 }
